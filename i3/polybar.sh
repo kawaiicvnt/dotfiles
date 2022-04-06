84c255187fcb8b08836a2f677eb87ebe6d063c89
Omp1 <evan3bal@gmail.com>
2022-04-06 22:54:45 +0300

    Changes to i3/config, added picom

--- i3/polybar.sh
+++ i3/polybar.sh
@@ -11,12 +11,12 @@ fi
 
 
 if type "xrandr"; then
-  count=1
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    #if [ $count -lt 3 ] ; then
-    MONITOR=$m polybar --reload main -c /home/v4ngbz/.config/polybar/blocks/config.ini &
-    #fi
-    let "count += 1";
+    if ! [ $m == DP-1 ] ; then
+    	MONITOR=$m polybar --reload main -c /home/v4ngbz/.config/polybar/blocks/config.ini &
+    else
+        MONITOR=$m polybar --reload main -c /home/v4ngbz/.config/polybar/blocks/config2.ini &
+    fi
     echo $count
   done
 else
