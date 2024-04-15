40825f87d550f0d9cf4a3b407c7799e388c9669b
Evie <iviballou@gmail.com>
2024-04-15 12:53:11 +0300

    barebones setup script

---
+++ .setup.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+echo "make sure this is run in and from your dotfiles directory"
+
+# link folders / files
+ln -s "$PWD/.zshrc"  "$HOME/.zshrc"
+ln -s "$PWD/dunst"   "$HOME/.config/dunst"
+ln -s "$PWD/i3"      "$HOME/.config/i3"
+ln -s "$PWD/kitty"   "$HOME/.config/kitty"
+ln -s "$PWD/picom"   "$HOME/.config/picom"
+ln -s "$PWD/polybar" "$HOME/.config/polybar"
+ln -s "$PWD/thunar"  "$HOME/.config/thunar"
+
+# install needed bins w/ pacman
+sudo pacman -Sy dunst i3-wm kitty picom polybar thunar rofi zsh
+
+# install pipewire and audio stuff
+sudo pacman -Sy pipewire pipewire-pulse alsa alsa-utils pavucontrol
+
+paru -Sy smart-playerctl-git uwufetch
+
+# make github directory and clone needed repos
+mkdir -p $HOME/.git/github/
+cd $HOME/.git/github/
+git clone https://github.com/Jvanrhijn/polybar-spotify.git
+git clone https://github.com/haideralipunjabi/polybar-browsermediacontrol.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
