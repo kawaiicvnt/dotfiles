af1aa3620ce83cf0751d8f9a3218938f129638da
Evie (Ivi) Ballou <iviballou@gmail.com>
2025-09-17 15:48:50 +0300

    conditionaly aliasing kitty ssh

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,19 @@
 if status is-interactive
 	# Commands to run in interactive sessions can go here
-	macchina | sed "s/\( Type1ProductConfigId\| 103C_5335M8\)//g" | sed "s/ 13-/ 13-bf0xxx/g"| sed "s/13-bf0xxxbf0xxx/13-bf0xxx/g"
-	echo
-	df -hT -t ext4 -t fuseblk | grep -v '/boot'
+	#macchina | sed "s/\( Type1ProductConfigId\| 103C_5335M8\)//g" | sed "s/ 13-/ 13-bf0xxx/g"| sed "s/13-bf0xxxbf0xxx/13-bf0xxx/g"
+	#echo
+	df -hT -t btrfs -t vfat
 end
 
-alias ll "ls -l"
-alias la "ls -la"
+if test $TERM = "xterm-kitty"
+      alias ssh="kitty +kitten $(which ssh)"	
+end
+
+# alias ssh="TERM=xterm-256color $(which ssh)"
+
+alias ll="ls -l"
+alias la="ls -la"
 alias sl='exec dbus-launch sway -c ~/.config/sway/config'
-alias ssh="TERM=xterm-256color $(which ssh)"
 alias spw="startplasma-wayland"
 alias i3l="xinit -- :0 vt2"
 alias ffmpreg="ffmpeg"
