40a7b9b088a89514c5739e7423ffc493f2a639cc
Evie (Ivi) Ballou <iviballou@gmail.com>
2025-10-11 22:12:47 +0300

    separate history between hosts and toolboxes

--- fish/config.fish
+++ fish/config.fish
@@ -5,16 +5,27 @@ if status is-interactive
 	df -hT -t btrfs -t vfat
 end
 
+# If the term is kitty and we're not in a toolbox, do make sure we spawn a kitten when ssh-ing.
 if test $TERM = "xterm-kitty"
-      alias ssh="kitty +kitten ssh"	
+    and test $HOSTNAME != "toolbx"
+  alias ssh="kitty +kitten ssh"	
+end
+
+# Check if we're in a toolbox
+if test -e "/run/.containerenv"
+    and test $HOSTNAME = "toolbx"
+  set -x fish_history tbx_$(grep "name=\"" /run/.containerenv | sed 's/name=\"//; s/.$//')
+else
+  set -x fish_history $HOSTNAME
 end
 
 set -gxa PATH ~/.nix-profile/bin
+set -gxa PATH ~/.local/bin
 
 # alias ssh="TERM=xterm-256color $(which ssh)"
 
-alias ll="ls -l"
-alias la="ls -la"
+alias ll="ls -lahGA --group-directories-first"
+alias la="ls -lahGA --group-directories-first"
 alias sl='exec dbus-launch sway -c ~/.config/sway/config'
 alias spw="startplasma-wayland"
 alias i3l="xinit -- :0 vt2"
