c220ce4b3c678be48e5e4ce8049bbdae2482d620
Evie (Ivi) Ballou <iviballou@gmail.com>
2025-11-06 01:03:16 +0200

    nvim: increased nvim-tree default width from 20 to 30

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -11,7 +11,7 @@ nvim_tree.setup {
   sort_by = "name",
   update_cwd = false,
   view = {
-    width = 20,
+    width = 30,
     side = "left",
     preserve_window_proportions = false,
     number = true, -- changed
